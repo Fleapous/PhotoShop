99923088a579e2865f569ba7be7d2b1e4eeb353d
egecan giray <girayegecan02@gmail.com>
2024-02-25 22:35:00 +0100

    Function filters done

--- PhotoShop/FunctionFilters.cs
+++ PhotoShop/FunctionFilters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +13,67 @@ namespace PhotoShop
 {
     internal class FunctionFilters
     {
-        public static WriteableBitmap Inversion(WriteableBitmap BitmapIn)
-        {
-            int width = BitmapIn.PixelWidth;
-            int height = BitmapIn.PixelHeight;
-            int stride = width * ((BitmapIn.Format.BitsPerPixel + 7) / 8);
+        private delegate byte FilterLogic(byte channel);
 
-            byte[] pixels = new byte[height * stride];
+        private static byte InversionLogic(byte pixel)
+        {
+            return (byte)(255 - pixel);
+        }
+        public static WriteableBitmap Inversion(WriteableBitmap bitmapIn)
+        {
+            return ApplyLogic(bitmapIn, InversionLogic);
+        }
+
+        private static byte BrightnessLogic(byte pixel)
+        {
+            return (byte)Math.Clamp(pixel + 30, 0, 255);
+        }
+        public static WriteableBitmap Brightness(WriteableBitmap bitmapIn)
+        {
+            return ApplyLogic(bitmapIn, BrightnessLogic);
+        }
+
+        private static byte ContrastLogic(byte pixel)
+        {
+            return (byte)Math.Clamp(pixel * 3, 0, 255);
+        }
+        public static WriteableBitmap Contrast(WriteableBitmap bitmapIn)
+        {
+            return ApplyLogic(bitmapIn, ContrastLogic);
+        }
+
+        private static byte GammaLogic(byte pixel)
+        {
+            double gamma = 1.5;
+            double normalizedpx = pixel / 255.0;
+            double gammaCorrectedPx = Math.Pow(normalizedpx, gamma) * 255.0;
+            return (byte)Math.Clamp(gammaCorrectedPx, 0, 255);
+        }
+        public static WriteableBitmap Gamma(WriteableBitmap bitmapIn)
+        {
+            return ApplyLogic(bitmapIn, GammaLogic);
+        }
+
+        private static WriteableBitmap ApplyLogic(WriteableBitmap BitmapIn, FilterLogic filterLogic)
+        {
+            int pxWidth = (int)BitmapIn.Width;
+            int pxHeight = (int)BitmapIn.Height;
+
+            int bytesPerPixel = (BitmapIn.Format.BitsPerPixel + 7) / 8;
+            int stride = (int)(pxWidth * bytesPerPixel);
+
+            Byte[] pixels = new Byte[stride * pxHeight];
             BitmapIn.CopyPixels(pixels, stride, 0);
 
-            for (int i = 0; i < pixels.Length; i += 4)
+            for (int i = 0; i < pixels.Length; i += bytesPerPixel)
             {
-                byte invertedR = (byte)(255 - pixels[i + 2]);
-                byte invertedG = (byte)(255 - pixels[i + 1]);
-                byte invertedB = (byte)(255 - pixels[i]);     
-
-                pixels[i + 2] = invertedR;
-                pixels[i + 1] = invertedG;
-                pixels[i] = invertedB;
+                pixels[i] = filterLogic(pixels[i]);
+                pixels[i + 1] = filterLogic(pixels[i + 1]);
+                pixels[i + 2] = filterLogic(pixels[i + 2]);
             }
+            BitmapIn.WritePixels(new Int32Rect(0, 0, pxWidth, pxHeight), pixels, stride, 0);
 
-            WriteableBitmap result = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
-            result.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride, 0);
-
-            return result;
+            return BitmapIn;
         }
 
         public static WriteableBitmap ApplyFilters(List<Func<WriteableBitmap, WriteableBitmap>> filters, WriteableBitmap originalImg)
--- PhotoShop/MainWindow.xaml.cs
+++ PhotoShop/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace PhotoShop
         private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.Filter = "Image Files (*.png;*.bmp)|*.png;*.bmp";
+            dialog.Filter = "Image Files (*.png;*.bmp;*.jpg)|*.png;*.bmp;*.jpg";
             if (dialog.ShowDialog() == true) //img sellected
             {
                 string imgPath = dialog.FileName;
@@ -61,6 +61,72 @@ namespace PhotoShop
                 filtersToApply.Remove(FunctionFilters.Inversion);
             }
                 
+            secondWindowImage.Source = FunctionFilters.ApplyFilters(filtersToApply, originalBitmap);
+        }
+
+        private void BrightnessButtonClick(object sender, RoutedEventArgs e)
+        {
+            //checking if bitmap is opened
+            if (originalBitmap == null)
+            {
+                MessageBox.Show("Please select an image before applying filters.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                BrightnessToggle.IsChecked = false;
+                return;
+            }
+
+            if (BrightnessToggle.IsChecked == true)
+            {
+                filtersToApply.Add(FunctionFilters.Brightness);
+            }
+            else if (BrightnessToggle.IsChecked == false)
+            {
+                filtersToApply.Remove(FunctionFilters.Brightness);
+            }
+
+            secondWindowImage.Source = FunctionFilters.ApplyFilters(filtersToApply, originalBitmap);
+        }
+
+        private void ContrastButtonClick(object sender, RoutedEventArgs e)
+        {
+            //checking if bitmap is opened
+            if (originalBitmap == null)
+            {
+                MessageBox.Show("Please select an image before applying filters.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Contrasttoggle.IsChecked = false;
+                return;
+            }
+
+            if (Contrasttoggle.IsChecked == true)
+            {
+                filtersToApply.Add(FunctionFilters.Contrast);
+            }
+            else if (Contrasttoggle.IsChecked == false)
+            {
+                filtersToApply.Remove(FunctionFilters.Contrast);
+            }
+
+            secondWindowImage.Source = FunctionFilters.ApplyFilters(filtersToApply, originalBitmap);
+        }
+
+        private void GammaButtonClick(object sender, RoutedEventArgs e)
+        {
+            //checking if bitmap is opened
+            if (originalBitmap == null)
+            {
+                MessageBox.Show("Please select an image before applying filters.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                GammaToggle.IsChecked = false;
+                return;
+            }
+
+            if (GammaToggle.IsChecked == true)
+            {
+                filtersToApply.Add(FunctionFilters.Gamma);
+            }
+            else if (GammaToggle.IsChecked == false)
+            {
+                filtersToApply.Remove(FunctionFilters.Ga);
+            }
+
             secondWindowImage.Source = FunctionFilters.ApplyFilters(filtersToApply, originalBitmap);
         }
     }
